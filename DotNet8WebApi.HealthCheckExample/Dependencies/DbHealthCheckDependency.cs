e611a4b1f6def0aca564c44b3e7ed0c48b9399c4
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-26 18:30:17 +0630

    -

--- DotNet8WebApi.HealthCheckExample/Dependencies/DbHealthCheckDependency.cs
+++ DotNet8WebApi.HealthCheckExample/Dependencies/DbHealthCheckDependency.cs
@@ -1,26 +1,33 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿namespace DotNet8WebApi.HealthCheckExample.Dependencies;
 
-namespace DotNet8WebApi.HealthCheckExample.Dependencies
+public static class DbHealthCheckDependency
 {
-    public static class DbHealthCheckDependency
+    public static IServiceCollection ConfigureHealthChecks(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        public static IServiceCollection ConfigureHealthChecks(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddHealthChecks()
-                .AddSqlServer(builder.Configuration.GetConnectionString("DbConnection")!, healthQuery: "select 1", name: "SQL Server", failureStatus: HealthStatus.Unhealthy, tags: new[] { "Feedback", "Database" });
+        services
+            .AddHealthChecks()
+            .AddSqlServer(
+                builder.Configuration.GetConnectionString("DbConnection")!,
+                healthQuery: "select 1",
+                name: "SQL Server",
+                failureStatus: HealthStatus.Unhealthy,
+                tags: new[] { "Feedback", "Database" }
+            );
 
-            //services.AddHealthChecksUI();
-            services.AddHealthChecksUI(opt =>
+        //services.AddHealthChecksUI();
+        services
+            .AddHealthChecksUI(opt =>
             {
-                opt.SetEvaluationTimeInSeconds(10); //time in seconds between check    
-                opt.MaximumHistoryEntriesPerEndpoint(60); //maximum history of checks    
-                opt.SetApiMaxActiveRequests(1); //api requests concurrency    
-                opt.AddHealthCheckEndpoint("Sample API", "/api/health"); //map health check api    
-
+                opt.SetEvaluationTimeInSeconds(10); //time in seconds between check
+                opt.MaximumHistoryEntriesPerEndpoint(60); //maximum history of checks
+                opt.SetApiMaxActiveRequests(1); //api requests concurrency
+                opt.AddHealthCheckEndpoint("Sample API", "/api/health"); //map health check api
             })
-                .AddInMemoryStorage();
+            .AddInMemoryStorage();
 
-            return services;
-        }
+        return services;
     }
 }
