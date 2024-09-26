849f37fe9d8b2b1b18a431ba046d67bc21b95fed
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-26 20:09:31 +0630

    [ Complete ]

--- DotNet8WebApi.HealthCheckExample/GlobalUsings.cs
+++ DotNet8WebApi.HealthCheckExample/GlobalUsings.cs
@@ -1,1 +1,5 @@
 ﻿global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using DotNet8WebApi.HealthCheckExample.Dependencies;
+global using HealthChecks.UI.Client;
+global using HealthChecks.UI.Configuration;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
--- DotNet8WebApi.HealthCheckExample/Program.cs
+++ DotNet8WebApi.HealthCheckExample/Program.cs
@@ -1,14 +1,6 @@
-using DotNet8WebApi.HealthCheckExample.Dependencies;
-using HealthChecks.UI.Client;
-using HealthChecks.UI.Configuration;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,7 +10,6 @@ builder.Services.ConfigureHealthChecks(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,17 +21,21 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 //HealthCheck Middleware
-app.MapHealthChecks("/api/health", new HealthCheckOptions()
-{
-    Predicate = _ => true,
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-app.UseHealthChecksUI(delegate (Options options)
-{
-    options.UIPath = "/healthcheck-ui";
-    //options.AddCustomStylesheet("./HealthCheck/Custom.css");
-
-});
+app.MapHealthChecks(
+    "/api/health",
+    new HealthCheckOptions()
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    }
+);
+app.UseHealthChecksUI(
+    delegate(Options options)
+    {
+        options.UIPath = "/healthcheck-ui";
+        //options.AddCustomStylesheet("./HealthCheck/Custom.css");
+    }
+);
 
 app.MapControllers();
 
