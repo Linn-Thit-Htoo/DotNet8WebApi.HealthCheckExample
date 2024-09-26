0e268501401a204047c0c5f813708c64f0953e52
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-26 18:27:18 +0630

    Add project files.

---
+++ DotNet8WebApi.HealthCheckExample/Dependencies/DbHealthCheckDependency.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace DotNet8WebApi.HealthCheckExample.Dependencies
+{
+    public static class DbHealthCheckDependency
+    {
+        public static IServiceCollection ConfigureHealthChecks(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddHealthChecks()
+                .AddSqlServer(builder.Configuration.GetConnectionString("DbConnection")!, healthQuery: "select 1", name: "SQL Server", failureStatus: HealthStatus.Unhealthy, tags: new[] { "Feedback", "Database" });
+
+            //services.AddHealthChecksUI();
+            services.AddHealthChecksUI(opt =>
+            {
+                opt.SetEvaluationTimeInSeconds(10); //time in seconds between check    
+                opt.MaximumHistoryEntriesPerEndpoint(60); //maximum history of checks    
+                opt.SetApiMaxActiveRequests(1); //api requests concurrency    
+                opt.AddHealthCheckEndpoint("Sample API", "/api/health"); //map health check api    
+
+            })
+                .AddInMemoryStorage();
+
+            return services;
+        }
+    }
+}
---
+++ DotNet8WebApi.HealthCheckExample/Program.cs
@@ -0,0 +1,47 @@
+using DotNet8WebApi.HealthCheckExample.Dependencies;
+using HealthChecks.UI.Client;
+using HealthChecks.UI.Configuration;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks();
+
+builder.Services.ConfigureHealthChecks(builder);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//HealthCheck Middleware
+app.MapHealthChecks("/api/health", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.UseHealthChecksUI(delegate (Options options)
+{
+    options.UIPath = "/healthcheck-ui";
+    //options.AddCustomStylesheet("./HealthCheck/Custom.css");
+
+});
+
+app.MapControllers();
+
+app.Run();
